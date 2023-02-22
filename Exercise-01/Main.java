873c01ff3ef0d6c29e86ddcd32a1462f123f0980
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2023-02-22 17:09:47 +0000

    Exercise-01

---
+++ Exercise-01/Main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    QueueX mainQueue = new QueueX(5);
+    QueueX evenQueue = new QueueX(5);
+    QueueX oddQueue = new QueueX(5);
+    int count = 0;
+    int num;
+
+    int evenCount = 0;
+    int oddCount = 0;
+
+    for (int i = 0; i < 5; i++) {
+      System.out.print("Enter transaction ID " + (i + 1) + ": ");
+      num = scanner.nextInt();
+      mainQueue.insert(num);
+
+      if (num % 2 == 0) {
+        evenQueue.insert(num);
+      } else {
+        oddQueue.insert(num);
+      }
+    }
+
+    System.out.println("");
+    System.out.println("PC1");
+    while (!evenQueue.isEmpty()) {
+      System.out.println("Transaction: " + evenQueue.remove());
+    }
+
+    System.out.println("");
+    System.out.println("PC2");
+    while (!oddQueue.isEmpty()) {
+      System.out.println("Transaction: " + oddQueue.remove());
+    }
+  }
+}
