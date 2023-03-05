d980c549043d6c8dd452573df8012b540b249755
Diwan Sachidu <45502921+sachidumaleesha@users.noreply.github.com>
2023-03-06 00:18:53 +0530

    Create the main class

---
+++ Exercise-04/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+      decimalconvert d = new decimalconvert();
+      d.decimalget();
+      d.binaryconvert();
+      d.displayQueue();
+}
+}
